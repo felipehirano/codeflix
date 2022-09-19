039a942075e52daaf76dc4404f0d185a48761a49
Felipe Ken <fkenhirano4@gmail.com>
2022-09-19 17:21:48 +0000

    Configurado zsh com powerlevel10k para facilitar o desenvolvimento

--- src/category/domain/entities/category.ts
+++ src/category/domain/entities/category.ts
@@ -1,5 +1,5 @@
 // Objeto Valor
-import UniqueEntityId from "../../../@seedwork/domain/unique-entity-id.vo";
+import UniqueEntityId from "../../../@seedwork/domain/value-objects/unique-entity-id.vo";
 
 export type CategoryProperties = {
   name: string;
